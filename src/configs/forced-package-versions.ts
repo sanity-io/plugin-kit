a01dfcdf31d22e16d7425a63abb7c905e3a5ee17
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-10-31 12:43:56 +0100

    fix: use correct @sanity/ui package name

--- src/configs/forced-package-versions.ts
+++ src/configs/forced-package-versions.ts
@@ -4,7 +4,7 @@ export const forcedPackageVersions = {
   'react-dom': '^18.0.0',
   '@types/react-dom': '^18.0.0',
   sanity: 'dev-preview || 3.0.0-dev-preview.22',
-  'sanity-ui': '^1.0.0-beta.31',
+  '@sanity/ui': '^1.0.0-beta.31',
 
   //TODO delete both of these when we ditch parcel
   parcel: '~2.6.0',
