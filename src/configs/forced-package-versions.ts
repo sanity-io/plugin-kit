cb46dd9236d8b735243e5da0ddd363e4e526fe10
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-02 13:31:33 +0100

    fix: pins sanity to dev-preview || 3.0.0-rc.0

--- src/configs/forced-package-versions.ts
+++ src/configs/forced-package-versions.ts
@@ -3,6 +3,6 @@ export const forcedPackageVersions = {
   '@types/react': '^18.0.0',
   'react-dom': '^18.0.0',
   '@types/react-dom': '^18.0.0',
-  sanity: 'dev-preview || 3.0.0-dev-preview.22',
+  sanity: 'dev-preview || 3.0.0-rc.0',
   '@sanity/ui': '^1.0.0-beta.31',
 } as const
