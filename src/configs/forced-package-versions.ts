9f2c6c65dda75d6d09639935609c391b3237fc57
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-11-30 21:26:05 +0100

    fix: use sanity ^3.0.0 and @sanity/ui ^1.0.0 for plugins

--- src/configs/forced-package-versions.ts
+++ src/configs/forced-package-versions.ts
@@ -1,16 +1,12 @@
-const sanityVersion = '3.0.0-rc.2'
+const sanityVersion = '^3.0.0'
 
 export const forcedPackageVersions = {
   react: '^18',
   '@types/react': '^18',
   'react-dom': '^18',
   '@types/react-dom': '^18',
-  sanity: `dev-preview || ${sanityVersion}`,
-  '@sanity/ui': '1.0.0-beta.32',
+  sanity: sanityVersion,
+  '@sanity/ui': '^1.0.0',
 }
 
-export const forcedDevPackageVersions = {
-  ...forcedPackageVersions,
-  // tagged release does not play nice with npm ci
-  sanity: sanityVersion,
-}
+export const forcedDevPackageVersions = forcedPackageVersions
